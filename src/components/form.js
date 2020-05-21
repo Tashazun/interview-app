8134f449cab53cef11504cf67d75106cc6a4e069
Tasha Zuniga <tashazun@gmail.com>
2020-05-21 16:09:06 -0700

    basic setup of form and state values

---
+++ src/components/form.js
@@ -0,0 +1,37 @@
+import React from "react"
+import useForm from "./useForm";
+
+const Form = () => {
+  const { values, handleChange, handleSubmit } = useForm(login);
+
+  function login() {
+    console.log(values);
+  }
+ 
+  return (
+    <div className="form">
+      <h2 className="form__title">Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          name="username"
+          onChange={handleChange}
+          value={values.username || ""}
+          required />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          onChange={handleChange}
+          value={values.password || ""}
+          required />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Form
