7dd02e3f53e749094a3f3789cc6d1fb45450d31b
Tasha Zuniga <tashazun@gmail.com>
2020-05-21 17:33:24 -0700

    styling plugin added to config

--- src/components/form.js
+++ src/components/form.js
@@ -2,20 +2,23 @@ import React from "react"
 import useForm from "./useForm";
 import validate from './validation';
 
+import "./form.scss";
+
 const Form = () => {
-  const { values, handleChange, handleSubmit } = useForm(login, validate);
+  const { values, errors, handleChange, handleSubmit } = useForm(login, validate);
 
   function login() {
     console.log(values);
   }
  
   return (
-    <div className="form">
-      <h2 className="form__title">Login</h2>
+    <div className="login">
+      <h2 className="login__title">Login</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username</label>
         <input
           id="username"
+          className={`login__username ${errors.username && 'login__username--invalid'}`}
           type="text"
           name="username"
           onChange={handleChange}
@@ -24,6 +27,7 @@ const Form = () => {
         <label htmlFor="password">Password</label>
         <input
           id="password"
+          className={`login__password ${errors.password && 'login__password--invalid'}`}
           type="password"
           name="password"
           onChange={handleChange}
